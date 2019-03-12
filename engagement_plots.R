9ee51e84d8597eaf9082cdabc3791cfdcd5b1f47
Sean Hardison <seanhardison@gmail.com>
2019-03-11 22:22:31 -0400

    working through protected species

--- engagement_plots.R
+++ engagement_plots.R
@@ -1,15 +1,20 @@
-engagement_plots <- function(epu, type){
+engagement_plots <- function(epu, type, leg = T, make_sparse = F){
   
   if (epu == "MAB"){
     xmin = -78
     xmax = -70
     ymin = 35
     ymax = 42
-  } else {
+  } else if (epu %in% c("GOM","GB")){
     xmin = -76
     xmax = -66
     ymin = 40
     ymax = 46
+  } else if (epu == "shelf"){
+    ymin <- 35
+    ymax <- 46
+    xmin <- -78
+    xmax <- -66
   }
   
   xlims <- c(xmin, xmax)
@@ -21,9 +26,11 @@ engagement_plots <- function(epu, type){
   if (epu == "MAB"){
     eng_rel <- ecodata::eng_rel %>%
       filter(PRIMARY_LATITUDE < 42)
-  } else {
+  }  else if (epu %in% c("GOM","GB")){
     eng_rel <- ecodata::eng_rel %>%
       filter(PRIMARY_LATITUDE > 40)
+  } else if (epu == "shelf"){
+    eng_rel <- ecodata::eng_rel
   }
   eng_rel <- eng_rel %>% 
     dplyr::rename(comeng = ComEng_NE16_ct,
@@ -54,6 +61,16 @@ engagement_plots <- function(epu, type){
     size <- "recrel"
   }
   
+  if (make_sparse){
+    if (type == "comm"){
+      eng_rel <- eng_rel %>% filter(comrel != 1,
+                                    comeng != 1)
+    } else if (type == "rec"){
+      eng_rel <- eng_rel %>% filter(recrel != 1,
+                                    receng != 1)
+    }
+  }
+  
 out <- eng_rel %>% 
       ggplot() +
       geom_sf(data = coast, size = map.lwd) +
@@ -62,11 +79,12 @@ out <- eng_rel %>%
       geom_point(aes(x = Longitude, y = Latitude,
                      fill = get(fill), size = get(size)),
                  color = "black",pch = 21) +
-      guides(fill = guide_legend(override.aes = list(size = 5),
-                                 reverse = T,
-                                 title = "Engagement"),
-             size = guide_legend(reverse = T,
-                                 title = "Reliance")) +
+      {if (!leg) guides(fill = F, size = F) else
+        guides(fill = guide_legend(override.aes = list(size = 5),
+                                      reverse = T,
+                                    title = "Engagement"),
+                size = guide_legend(reverse = T,
+                                    title = "Reliance"))} +
       scale_fill_manual(values = biv_col) +
       theme_map() +
       xlab("Longitude") +
